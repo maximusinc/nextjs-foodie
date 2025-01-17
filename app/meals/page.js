e8e9b0aa98f65b0b7524beaf3f8e98d6d5d97c08
Maxim Kirdeev <maxim@shoretechnologygroup.com>
2025-01-17 12:47:04 +0400

    initial commit

---
+++ app/meals/page.js
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import styles from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by community',
+};
+
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+
+
+  return (
+    <>
+      <header className={styles.header} >
+        <h1>
+          Delicious meals, created{' '}
+          <span className={styles.highlight}>by you.</span>
+        </h1>
+        <p>Choose your recipe and cook it yourself. It is easy and fun.</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main} >
+        <Suspense fallback={<p className={styles.loading} >Fetching meals...</p>} >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+      <p>We could not find page</p>
+    </main>
+  );
+}
